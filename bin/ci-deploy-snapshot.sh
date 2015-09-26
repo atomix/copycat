710044cc13939ccfe0c1c9c3dafb31a83370b715
Jonathan Halterman <jhalterman@gmail.com>
2015-09-25 17:37:23 -0700

    Add post build snapshot deployment to travis

---
+++ bin/ci-deploy-snapshot.sh
@@ -0,0 +1,14 @@
+# From https://coderwall.com/p/9b_lfq
+
+REPO="atomix/catalogue"
+
+if [ "$TRAVIS_REPO_SLUG" == "$REPO" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo -e "Publishing maven snapshot...\n"
+
+  mvn clean source:jar deploy --settings="bin/settings.xml" -DskipTests=true -Dmaven.javadoc.skip=true
+
+  echo -e "Published maven snapshot"
+fi
