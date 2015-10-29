2241682e2ba77eea4afa35dbaeba99f5881ee847
Jonathan Halterman <jhalterman@gmail.com>
2015-10-29 11:33:05 -0700

    Added a new testing module to house testing infrastructure. Made test module not build a test jar, since nothing else is using it. Moved testing related dependencies from parent pom to testing module.

---
+++ testing/src/main/java/io/atomix/copycat/testing/TestCaseLogger.java
@@ -0,0 +1,27 @@
+package io.atomix.copycat.testing;
+
+import org.slf4j.LoggerFactory;
+import org.testng.IClass;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+
+public class TestCaseLogger implements IInvokedMethodListener {
+  private void logInvocation(String state, IInvokedMethod method) {
+    if (!method.isTestMethod())
+      return;
+
+    ITestNGMethod testMethod = method.getTestMethod();
+    IClass clazz = testMethod.getTestClass();
+    LoggerFactory.getLogger(state).info("{}.{}", clazz.getRealClass().getName(), testMethod.getMethodName());
+  }
+
+  public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+    logInvocation("BEFORE", method);
+  }
+
+  public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+    // logInvocation("AFTER", method);
+  }
+}
