70c5850417d2e0816e6c0876b9cb3b48ecd0fd61
Jordan Halterman <jordan.halterman@gmail.com>
2015-12-29 00:47:48 -0800

    Add client session submitter tests.

--- client/src/main/java/io/atomix/copycat/client/session/ClientSessionSubmitter.java
+++ client/src/main/java/io/atomix/copycat/client/session/ClientSessionSubmitter.java
@@ -17,7 +17,6 @@ package io.atomix.copycat.client.session;
 
 import io.atomix.catalyst.transport.Connection;
 import io.atomix.catalyst.util.Assert;
-import io.atomix.catalyst.util.concurrent.Scheduled;
 import io.atomix.catalyst.util.concurrent.ThreadContext;
 import io.atomix.copycat.client.Command;
 import io.atomix.copycat.client.Operation;
@@ -35,8 +34,6 @@ import io.atomix.copycat.client.response.Response;
 import io.atomix.copycat.client.util.ClientSequencer;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
@@ -51,9 +48,6 @@ public class ClientSessionSubmitter {
   private final ThreadContext context;
   private final RetryStrategy strategy;
   private final ClientSequencer sequencer = new ClientSequencer();
-  private final Map<Long, OperationAttempt> attempts = new HashMap<>();
-  private final Map<Long, Scheduled> retries = new HashMap<>();
-  private boolean open;
 
   public ClientSessionSubmitter(Connection connection, ClientSessionState state, ThreadContext context, RetryStrategy retryStrategy) {
     this.connection = Assert.notNull(connection, "connection");
@@ -91,9 +85,7 @@ public class ClientSessionSubmitter {
    * Submits a command request to the cluster.
    */
   private <T> void submitCommand(CommandRequest request, CompletableFuture<T> future) {
-    CommandAttempt<T> attempt = new CommandAttempt<>(sequencer.nextSequence(), request, future);
-    attempts.put(attempt.sequence, attempt);
-    submit(attempt);
+    submit(new CommandAttempt<>(sequencer.nextSequence(), request, future));
   }
 
   /**
@@ -136,9 +128,7 @@ public class ClientSessionSubmitter {
    * Submits a query request to the cluster.
    */
   private <T> void submitQuery(QueryRequest request, CompletableFuture<T> future) {
-    QueryAttempt<T> attempt = new QueryAttempt<>(sequencer.nextSequence(), request, future);
-    attempts.put(attempt.sequence, attempt);
-    submit(attempt);
+    submit(new QueryAttempt<>(sequencer.nextSequence(), request, future));
   }
 
   /**
@@ -147,7 +137,7 @@ public class ClientSessionSubmitter {
    * @param attempt The attempt to submit.
    */
   private <T extends OperationRequest<T>, U extends OperationResponse<U>, V> void submit(OperationAttempt<T, U, V> attempt) {
-    if (!open) {
+    if (state.getState() == Session.State.CLOSED || state.getState() == Session.State.EXPIRED) {
       attempt.fail(new ClosedSessionException("session closed"));
     } else {
       connection.<T, U>send(attempt.request).whenComplete(attempt);
@@ -160,11 +150,7 @@ public class ClientSessionSubmitter {
    * @return A completable future to be completed with a list of pending operations.
    */
   public CompletableFuture<Void> close() {
-    CompletableFuture<Void> future = new CompletableFuture<>();
-    context.executor().execute(() -> {
-      open = false;
-    });
-    return future;
+    return CompletableFuture.completedFuture(null);
   }
 
   /**
@@ -197,7 +183,7 @@ public class ClientSessionSubmitter {
     public void accept(U response, Throwable error) {
       if (error == null) {
         if (response.status() == Response.Status.OK) {
-          complete(response);
+          sequence(() -> complete(response));
         } else {
           strategy.attemptFailed(this, response.error().createException());
         }
@@ -260,7 +246,7 @@ public class ClientSessionSubmitter {
 
     @Override
     public void retry(Duration after) {
-      retries.put(sequence, context.schedule(after, () -> submit(next())));
+      context.schedule(after, () -> submit(next()));
     }
   }
 
@@ -278,9 +264,7 @@ public class ClientSessionSubmitter {
 
     @Override
     protected OperationAttempt<CommandRequest, CommandResponse, T> next() {
-      CommandAttempt<T> attempt = new CommandAttempt<>(sequence, this.attempt + 1, request, future);
-      attempts.put(attempt.sequence, attempt);
-      return attempt;
+      return new CommandAttempt<>(sequence, this.attempt + 1, request, future);
     }
 
     @Override
@@ -292,13 +276,12 @@ public class ClientSessionSubmitter {
     @SuppressWarnings("unchecked")
     protected void complete(CommandResponse response) {
       state.setCommandResponse(request.sequence());
-      attempts.remove(sequence);
+      state.setResponseIndex(response.index());
       future.complete((T) response.result());
     }
 
     @Override
     protected void complete(Throwable error) {
-      attempts.remove(sequence);
       future.completeExceptionally(error);
     }
   }
@@ -317,9 +300,7 @@ public class ClientSessionSubmitter {
 
     @Override
     protected OperationAttempt<QueryRequest, QueryResponse, T> next() {
-      QueryAttempt<T> attempt = new QueryAttempt<>(sequence, this.attempt + 1, request, future);
-      attempts.put(attempt.sequence, attempt);
-      return attempt;
+      return new QueryAttempt<>(sequence, this.attempt + 1, request, future);
     }
 
     @Override
@@ -333,7 +314,6 @@ public class ClientSessionSubmitter {
       // If the query consistency level is CAUSAL, we can simply complete queries in sequential order.
       if (request.query().consistency() == Query.ConsistencyLevel.CAUSAL) {
         state.setResponseIndex(response.index());
-        attempts.remove(sequence);
         future.complete((T) response.result());
       }
       // If the query consistency level is strong, the query must be executed sequentially. In order to ensure responses
@@ -344,7 +324,6 @@ public class ClientSessionSubmitter {
           retry();
         } else {
           state.setResponseIndex(response.index());
-          attempts.remove(sequence);
           future.complete((T) response.result());
         }
       }
@@ -352,7 +331,6 @@ public class ClientSessionSubmitter {
 
     @Override
     protected void complete(Throwable error) {
-      attempts.remove(sequence);
       future.completeExceptionally(error);
     }
   }
--- client/src/test/java/io/atomix/copycat/client/session/ClientSessionSubmitterTest.java
+++ client/src/test/java/io/atomix/copycat/client/session/ClientSessionSubmitterTest.java
@@ -15,7 +15,25 @@
  */
 package io.atomix.copycat.client.session;
 
+import io.atomix.catalyst.transport.Connection;
+import io.atomix.catalyst.util.concurrent.ThreadContext;
+import io.atomix.copycat.client.Command;
+import io.atomix.copycat.client.Query;
+import io.atomix.copycat.client.RetryStrategies;
+import io.atomix.copycat.client.request.CommandRequest;
+import io.atomix.copycat.client.request.QueryRequest;
+import io.atomix.copycat.client.response.CommandResponse;
+import io.atomix.copycat.client.response.QueryResponse;
+import io.atomix.copycat.client.response.Response;
+import org.mockito.Mockito;
 import org.testng.annotations.Test;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
 
 /**
  * Client session submitter test.
@@ -24,4 +42,183 @@ import org.testng.annotations.Test;
  */
 @Test
 public class ClientSessionSubmitterTest {
+
+  /**
+   * Tests submitting a command to the cluster.
+   */
+  public void testSubmitCommand() throws Throwable {
+    Connection connection = mock(Connection.class);
+    when(connection.send(any(CommandRequest.class)))
+      .thenReturn(CompletableFuture.completedFuture(CommandResponse.builder()
+        .withStatus(Response.Status.OK)
+        .withIndex(10)
+        .withResult("Hello world!")
+        .build()));
+
+    ClientSessionState state = new ClientSessionState(UUID.randomUUID())
+      .setSessionId(1)
+      .setState(Session.State.OPEN);
+
+    Executor executor = new MockExecutor();
+    ThreadContext context = mock(ThreadContext.class);
+    when(context.executor()).thenReturn(executor);
+
+    ClientSessionSubmitter submitter = new ClientSessionSubmitter(connection, state, context, RetryStrategies.RETRY);
+    assertEquals(submitter.submit(new TestCommand()).get(), "Hello world!");
+    assertEquals(state.getCommandRequest(), 1);
+    assertEquals(state.getCommandResponse(), 1);
+    assertEquals(state.getResponseIndex(), 10);
+  }
+
+  /**
+   * Test resequencing a command response.
+   */
+  public void testResequenceCommand() throws Throwable {
+    CompletableFuture<CommandResponse> future1 = new CompletableFuture<>();
+    CompletableFuture<CommandResponse> future2 = new CompletableFuture<>();
+
+    Connection connection = mock(Connection.class);
+    Mockito.<CompletableFuture<CommandResponse>>when(connection.send(any(CommandRequest.class)))
+      .thenReturn(future1)
+      .thenReturn(future2);
+
+    ClientSessionState state = new ClientSessionState(UUID.randomUUID())
+      .setSessionId(1)
+      .setState(Session.State.OPEN);
+
+    Executor executor = new MockExecutor();
+    ThreadContext context = mock(ThreadContext.class);
+    when(context.executor()).thenReturn(executor);
+
+    ClientSessionSubmitter submitter = new ClientSessionSubmitter(connection, state, context, RetryStrategies.RETRY);
+
+    CompletableFuture<String> result1 = submitter.submit(new TestCommand());
+    CompletableFuture<String> result2 = submitter.submit(new TestCommand());
+
+    future2.complete(CommandResponse.builder()
+      .withStatus(Response.Status.OK)
+      .withIndex(10)
+      .withResult("Hello world again!")
+      .build());
+
+    assertEquals(state.getCommandRequest(), 2);
+    assertEquals(state.getCommandResponse(), 0);
+    assertEquals(state.getResponseIndex(), 1);
+
+    assertFalse(result1.isDone());
+    assertFalse(result2.isDone());
+
+    future1.complete(CommandResponse.builder()
+      .withStatus(Response.Status.OK)
+      .withIndex(9)
+      .withResult("Hello world!")
+      .build());
+
+    assertTrue(result1.isDone());
+    assertEquals(result1.get(), "Hello world!");
+    assertTrue(result2.isDone());
+    assertEquals(result2.get(), "Hello world again!");
+
+    assertEquals(state.getCommandRequest(), 2);
+    assertEquals(state.getCommandResponse(), 2);
+    assertEquals(state.getResponseIndex(), 10);
+  }
+
+  /**
+   * Tests submitting a query to the cluster.
+   */
+  public void testSubmitQuery() throws Throwable {
+    Connection connection = mock(Connection.class);
+    when(connection.send(any(QueryRequest.class)))
+      .thenReturn(CompletableFuture.completedFuture(QueryResponse.builder()
+        .withStatus(Response.Status.OK)
+        .withIndex(10)
+        .withResult("Hello world!")
+        .build()));
+
+    ClientSessionState state = new ClientSessionState(UUID.randomUUID())
+      .setSessionId(1)
+      .setState(Session.State.OPEN);
+
+    Executor executor = new MockExecutor();
+    ThreadContext context = mock(ThreadContext.class);
+    when(context.executor()).thenReturn(executor);
+
+    ClientSessionSubmitter submitter = new ClientSessionSubmitter(connection, state, context, RetryStrategies.RETRY);
+    assertEquals(submitter.submit(new TestQuery()).get(), "Hello world!");
+    assertEquals(state.getResponseIndex(), 10);
+  }
+
+  /**
+   * Tests resequencing a query response.
+   */
+  public void testResequenceQuery() throws Throwable {
+    CompletableFuture<QueryResponse> future1 = new CompletableFuture<>();
+    CompletableFuture<QueryResponse> future2 = new CompletableFuture<>();
+
+    Connection connection = mock(Connection.class);
+    Mockito.<CompletableFuture<QueryResponse>>when(connection.send(any(QueryRequest.class)))
+      .thenReturn(future1)
+      .thenReturn(future2);
+
+    ClientSessionState state = new ClientSessionState(UUID.randomUUID())
+      .setSessionId(1)
+      .setState(Session.State.OPEN);
+
+    Executor executor = new MockExecutor();
+    ThreadContext context = mock(ThreadContext.class);
+    when(context.executor()).thenReturn(executor);
+
+    ClientSessionSubmitter submitter = new ClientSessionSubmitter(connection, state, context, RetryStrategies.RETRY);
+
+    CompletableFuture<String> result1 = submitter.submit(new TestQuery());
+    CompletableFuture<String> result2 = submitter.submit(new TestQuery());
+
+    future2.complete(QueryResponse.builder()
+      .withStatus(Response.Status.OK)
+      .withIndex(10)
+      .withResult("Hello world again!")
+      .build());
+
+    assertEquals(state.getResponseIndex(), 1);
+
+    assertFalse(result1.isDone());
+    assertFalse(result2.isDone());
+
+    future1.complete(QueryResponse.builder()
+      .withStatus(Response.Status.OK)
+      .withIndex(9)
+      .withResult("Hello world!")
+      .build());
+
+    assertTrue(result1.isDone());
+    assertEquals(result1.get(), "Hello world!");
+    assertTrue(result2.isDone());
+    assertEquals(result2.get(), "Hello world again!");
+
+    assertEquals(state.getResponseIndex(), 10);
+  }
+
+  /**
+   * Test command.
+   */
+  private static class TestCommand implements Command<String> {
+  }
+
+  /**
+   * Test query.
+   */
+  private static class TestQuery implements Query<String> {
+  }
+
+  /**
+   * Mock executor.
+   */
+  private static class MockExecutor implements Executor {
+    @Override
+    public void execute(Runnable command) {
+      command.run();
+    }
+  }
+
 }
