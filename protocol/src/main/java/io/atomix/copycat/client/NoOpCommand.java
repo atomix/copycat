43a9d0ac7d3ae73c7171c6b4b84ac32e8f5f0427
Jordan Halterman <jordan.halterman@gmail.com>
2015-12-31 23:22:49 -0800

    Submit no-op command when normal command fails.

---
+++ protocol/src/main/java/io/atomix/copycat/client/NoOpCommand.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package io.atomix.copycat.client;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.CatalystSerializable;
+import io.atomix.catalyst.serializer.SerializeWith;
+import io.atomix.catalyst.serializer.Serializer;
+
+/**
+ * A special placeholder command.
+ *
+ * @author <a href="http://github.com/kuujo>Jordan Halterman</a>
+ */
+@SerializeWith(id=217)
+public class NoOpCommand implements Command<Void>, CatalystSerializable {
+  @Override
+  public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+  }
+
+  @Override
+  public void readObject(BufferInput<?> buffer, Serializer serializer) {
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName();
+  }
+
+}
