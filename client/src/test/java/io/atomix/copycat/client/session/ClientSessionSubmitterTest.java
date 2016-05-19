2350d3893e65be6364395178aee09d213430a3b1
Madan Jampani <madan@onlab.us>
2016-05-18 18:07:11 -0700

    Fixes the sequencing logic so that failed queries can be skipped over

--- client/src/test/java/io/atomix/copycat/client/session/ClientSessionSubmitterTest.java
+++ client/src/test/java/io/atomix/copycat/client/session/ClientSessionSubmitterTest.java
@@ -19,8 +19,10 @@ import io.atomix.catalyst.transport.Connection;
 import io.atomix.catalyst.util.concurrent.ThreadContext;
 import io.atomix.copycat.Command;
 import io.atomix.copycat.Query;
+import io.atomix.copycat.error.QueryException;
 import io.atomix.copycat.protocol.*;
 import io.atomix.copycat.session.Session;
+
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
 
@@ -191,6 +193,50 @@ public class ClientSessionSubmitterTest {
     assertEquals(result1.get(), "Hello world!");
     assertTrue(result2.isDone());
     assertEquals(result2.get(), "Hello world again!");
+
+    assertEquals(state.getResponseIndex(), 10);
+  }
+
+  /**
+   * Tests skipping over a failed query attempt.
+   */
+  public void testSkippingOverFailedQuery() throws Throwable {
+    CompletableFuture<QueryResponse> future1 = new CompletableFuture<>();
+    CompletableFuture<QueryResponse> future2 = new CompletableFuture<>();
+
+    Connection connection = mock(Connection.class);
+    Mockito.<CompletableFuture<QueryResponse>>when(connection.send(any(QueryRequest.class)))
+      .thenReturn(future1)
+      .thenReturn(future2);
+
+    ClientSessionState state = new ClientSessionState(UUID.randomUUID())
+      .setSessionId(1)
+      .setState(Session.State.OPEN);
+
+    Executor executor = new MockExecutor();
+    ThreadContext context = mock(ThreadContext.class);
+    when(context.executor()).thenReturn(executor);
+
+    ClientSessionSubmitter submitter = new ClientSessionSubmitter(connection, state, new ClientSequencer(state), context);
+
+    CompletableFuture<String> result1 = submitter.submit(new TestQuery());
+    CompletableFuture<String> result2 = submitter.submit(new TestQuery());
+
+    assertEquals(state.getResponseIndex(), 1);
+
+    assertFalse(result1.isDone());
+    assertFalse(result2.isDone());
+
+    future1.completeExceptionally(new QueryException("failure"));
+    future2.complete(QueryResponse.builder()
+        .withStatus(Response.Status.OK)
+        .withIndex(10)
+        .withResult("Hello world!")
+        .build());
+
+    assertTrue(result1.isCompletedExceptionally());
+    assertTrue(result2.isDone());
+    assertEquals(result2.get(), "Hello world!");
 
     assertEquals(state.getResponseIndex(), 10);
   }
